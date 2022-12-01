227835adef14f76441a6cc06db1bf97a8851b3a3
sspeabody <115673355+sspeabody@users.noreply.github.com>
2022-12-01 18:21:25 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function cambiarfondo() {document.body.style.backgroundImage = "none";
+                         document.body.style.backgroundColor = "#1A144C";
+                         document.body.style.color = "white"
+                         document.getElementById("text1").style.color = "black";
+                         document.getElementById("text2").style.color = "black";
+                         document.getElementById("text3").style.color = "black";
+                         document.getElementById("text4").style.color = "black";
+                         document.getElementById("cardtitle1").style.color = "black";
+                         document.getElementById("cardtitle2").style.color = "black";
+                         document.getElementById("cardtitle3").style.color = "black";
+                         document.getElementById("cardtitle4").style.color = "black";
+                         }
+
+function copy() {alert("Autora: Soledad Suárez Peabody. e-mail: soledad_suarez@hotmail.es");
+
+                }
